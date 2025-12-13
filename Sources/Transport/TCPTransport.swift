4be3e0dfc7b97fa1d21c71ccabb7a23eba69e0f0
kenykhuang <kenykhuang@kkcompany.com>
2025-12-13 14:58:13 +0800

    feat: tune TCP multipathServiceType

--- Sources/Transport/TCPTransport.swift
+++ Sources/Transport/TCPTransport.swift
@@ -81,6 +81,7 @@ public class TCPTransport: Transport {
             }, queue)
         }
         let parameters = NWParameters(tls: tlsOptions, tcp: options)
+        parameters.multipathServiceType = .handover
         let conn = NWConnection(host: NWEndpoint.Host.name(parts.host, nil), port: NWEndpoint.Port(rawValue: UInt16(parts.port))!, using: parameters)
         connection = conn
         start()
